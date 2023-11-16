288c7306edcfc45bafa817fbbeac36982de423e6
Jula <juliakonieczna15@gmail.com>
2023-11-16 11:36:01 +0100

    Wyskakujący kot po udanym zalogowaniu

--- app/src/main/java/com/example/firebaseloginapp/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseloginapp/LoginActivity.kt
@@ -23,7 +23,7 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this, AppCompatActivity::class.java))
+                    startActivity(Intent(this, CatActivity::class.java))
                 } else {
                     Toast.makeText(
                         this, "Unable to login. Check your input or try again later",
